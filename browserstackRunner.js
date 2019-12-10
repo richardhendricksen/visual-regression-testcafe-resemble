55bbbaaa55812acd406c5c6f2e04930f7f36f7e1
richardhendricksen <richard.hendricksen@gmail.com>
2019-12-10 21:16:23 +0100

    added exit code

--- browserstackRunner.js
+++ browserstackRunner.js
@@ -2,26 +2,29 @@ const createTestCafe = require('testcafe');
 let testcafe = null;
 
 createTestCafe('localhost', 1337, 1338)
-  .then(tc => {
-    testcafe = tc;
-    const runner = testcafe.createRunner();
+    .then(tc => {
+        testcafe = tc;
+        const runner = testcafe.createRunner();
 
-    process.env.BROWSERSTACK_USERNAME = '<MY_BROWSERSTACK_USERNAME>';
-    process.env.BROWSERSTACK_ACCESS_KEY = '<MY_BROWSERSTACK_ACCESS_KEY>';
+        process.env.BROWSERSTACK_USERNAME = '<MY_BROWSERSTACK_USERNAME>';
+        process.env.BROWSERSTACK_ACCESS_KEY = '<MY_BROWSERSTACK_ACCESS_KEY>';
 
-    //We need this for taking screenshots of elements
-    process.env.BROWSERSTACK_USE_AUTOMATE = '1';
+        //We need this for taking screenshots of elements
+        process.env.BROWSERSTACK_USE_AUTOMATE = '1';
 
-    return runner
-      .src(['tests/**/*.spec.ts'])
-      .browsers(['browserstack:chrome@78.0:Windows 10'])
-      .reporter(['spec', {
-        name: 'html',
-        output: 'reports/report.html'
-      }])
-      .run();
-  })
-  .then(failedCount => {
-    console.log('Tests failed: ' + failedCount);
-    testcafe.close();
-  });
+        return runner
+            .src(['tests/**/*.spec.ts'])
+            .browsers(['browserstack:chrome@78.0:Windows 10'])
+            .reporter(['spec', {
+                name: 'html',
+                output: 'reports/report.html'
+            }])
+            .run();
+    })
+    .then(failedCount => {
+        console.log('Tests failed: ' + failedCount);
+        testcafe.close();
+        if (failedCount > 0) {
+            process.exit(1);
+        }
+    });
--- ciRunner.js
+++ ciRunner.js
@@ -2,20 +2,23 @@ const createTestCafe = require('testcafe');
 let testcafe = null;
 
 createTestCafe('localhost', 1337, 1338)
-  .then(tc => {
-    testcafe = tc;
-    const runner = testcafe.createRunner();
+    .then(tc => {
+        testcafe = tc;
+        const runner = testcafe.createRunner();
 
-    return runner
-      .src(['tests/**/*.spec.ts'])
-      .browsers(['chrome:headless'])
-      .reporter(['spec', {
-        name: 'html',
-        output: 'reports/report.html'
-      }])
-      .run();
-  })
-  .then(failedCount => {
-    console.log('Tests failed: ' + failedCount);
-    testcafe.close();
-  });
+        return runner
+            .src(['tests/**/*.spec.ts'])
+            .browsers(['chrome:headless'])
+            .reporter(['spec', {
+                name: 'html',
+                output: 'reports/report.html'
+            }])
+            .run();
+    })
+    .then(failedCount => {
+        console.log('Tests failed: ' + failedCount);
+        testcafe.close();
+        if (failedCount > 0) {
+            process.exit(1);
+        }
+    });
