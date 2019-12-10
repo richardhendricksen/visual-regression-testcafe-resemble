7ac3cf6c4c48fdc44883106c3a56da1320ccacf0
richardhendricksen <richard.hendricksen@gmail.com>
2019-12-10 21:04:38 +0100

    fixed layout + added window size change

--- tests/todomvc.spec.ts
+++ tests/todomvc.spec.ts
@@ -5,15 +5,17 @@ import { Endpoints } from '../mocking/endpoints';
 import { RequestMockBuilder } from '../mocking/request-mock-builder';
 
 const mock = RequestMockBuilder.new()
-  .selectScenario(Endpoints.FIRST_ENDPOINT, 'default')
-  .build();
+    .selectScenario(Endpoints.FIRST_ENDPOINT, 'default')
+    .build();
 
 fixture(`todomvc`)
-  .page(`http://todomvc.com/examples/angular2/`)
-  .requestHooks(mock);
+    .page(`http://todomvc.com/examples/angular2/`)
+    .requestHooks(mock);
 
 test('Input field', async t => {
-  const inputFieldElement = Selector('body > todo-app > section > header > input');
 
-  await t.expect(await compareElement(t, inputFieldElement)).eql(0, 'Images are not equal');
+    await t.resizeWindow(1280, 1024);
+    const inputFieldElement = Selector('body > todo-app > section > header > input');
+
+    await t.expect(await compareElement(t, inputFieldElement)).lt(0.1, 'Images are not equal');
 });
