d1224269d9943b589f20937c7158e064aca92f1a
richardhendricksen <richard.hendricksen@gmail.com>
2019-12-10 20:46:00 +0100

    added runner

---
+++ browserstackRunner.js
@@ -0,0 +1,27 @@
+const createTestCafe = require('testcafe');
+let testcafe = null;
+
+createTestCafe('localhost', 1337, 1338)
+  .then(tc => {
+    testcafe = tc;
+    const runner = testcafe.createRunner();
+
+    process.env.BROWSERSTACK_USERNAME = '<BROWSERSTACK_USERNAME>';
+    process.env.BROWSERSTACK_ACCESS_KEY = '<MY_BROWSERSTACK_ACCESS_KEY>';
+
+    //We need this for taking screenshots of elements
+    process.env.BROWSERSTACK_USE_AUTOMATE = '1';
+
+    return runner
+      .src(['tests/**/*.spec.ts'])
+      .browsers(['browserstack:chrome@78.0:Windows 10'])
+      .reporter(['spec', {
+        name: 'html',
+        output: 'reports/report.html'
+      }])
+      .run();
+  })
+  .then(failedCount => {
+    console.log('Tests failed: ' + failedCount);
+    testcafe.close();
+  });
---
+++ ciRunner.js
@@ -0,0 +1,21 @@
+const createTestCafe = require('testcafe');
+let testcafe = null;
+
+createTestCafe('localhost', 1337, 1338)
+  .then(tc => {
+    testcafe = tc;
+    const runner = testcafe.createRunner();
+
+    return runner
+      .src(['tests/**/*.spec.ts'])
+      .browsers(['chrome', 'safari', 'firefox'])
+      .reporter(['spec', {
+        name: 'html',
+        output: 'reports/report.html'
+      }])
+      .run();
+  })
+  .then(failedCount => {
+    console.log('Tests failed: ' + failedCount);
+    testcafe.close();
+  });
